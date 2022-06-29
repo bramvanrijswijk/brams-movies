b8e5c1adc859810fa187b47e901e294824c528a1
Bram van Rijswijk <b.vanrijswijk@outlook.com>
2022-06-29 15:29:59 +0200

    Added transition when navigating between pages

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition
+      mode="out-in"
+      name="fade"
+    >
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
